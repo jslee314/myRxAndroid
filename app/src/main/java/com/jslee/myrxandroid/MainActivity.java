a3b2fd217265294ce1f7265e7aa34fd0e2fcd979
jslee314 <jslee_314@naver.com>
2021-08-21 18:09:55 +0900

    MainActivity

--- app/src/main/java/com/jslee/myrxandroid/MainActivity.java
+++ app/src/main/java/com/jslee/myrxandroid/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.jslee.myrxandroid.fragments.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(android.R.id.content, new MainFragment(), MainFragment.TAG)
+                    .commit();
+
+        }
+
     }
 }
